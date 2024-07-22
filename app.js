e2079bb04aaddb9ff308d0d9427f139ed83d45e1
jocyyyy <jccastaneda9719@gmail.com>
2024-07-22 16:50:12 +0000

    added js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const input = document.querySelector('input')
+const input = document.querySelector('.input')
 const result = document.querySelector('.result')
 const deleteBtn = document.querySelector('.delete')
 const keys = document.querySelectorAll('.bottom span')
@@ -11,4 +11,39 @@ let decimalAdded = false
 
 const operators = ['+', '-', 'x', '/']
 
-function handleKeyPress(e){}
+function handleKeyPress(e){
+    const key =e.target.dataset.key
+    const lastChar =operation[operation.length -1]
+
+    if (key==="="){
+        return
+    }
+
+    if (key === '.'&& decimalAdded){
+        return
+    }
+
+    if (opperators.index0f(key) !== -1){
+        decimalAdded = false
+    }
+
+    if (operation.length === 0 && key ==='-'){
+        operation+= key
+        input.innerHTML = operation
+        return
+    }
+
+    if (operation.length === 0 && operators.indexOf(key) !== -1){
+        input.innerHTML = operation
+        return
+    }
+
+    if (operators.indexOf(lastChar) !== -1 &&operators.indexOf(key) !== -1){
+operation = operation.replace(/.$/, key)
+input.innerHTML = operation 
+return
+    }
+
+
+
+}
